2605d211e5be66ddd77f30dd483d2d1a564d3d98
Amanda Souza <amanda.minueza@gmail.com>
2017-09-01 11:52:18 -0300

    test

--- test.c
+++ test.c
@@ -4,9 +4,26 @@
 
 int main(int argc, char **argv)
 {
-  MPI_Init(NULL, NULL);
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  printf("it works!\n");
-  MPI_Finalize();
+    // Testing MPI_INIT
+    if(MPI_Init(NULL, NULL) !=  MPI_SUCCESS){
+        printf("[INIT] it does not work!\n");
+    }
+    printf("[INIT] it works!\n");
+
+    // Testing MPI_RANK
+    int world_rank;
+    if(MPI_Comm_rank(MPI_COMM_WORLD, &world_rank) !=  MPI_SUCCESS){
+        printf("[RANK] it does not work!\n");
+    }
+    printf("[RANK] it works! Rank: %d\n", world_rank);
+
+    // Testing MPI_SIZE
+    int size;
+    if(MPI_Comm_size(MPI_COMM_WORLD, &size) !=  MPI_SUCCESS){
+        printf("[SIZE] it does not work!\n");
+    }
+    printf("[SIZE] it works! Size: %d\n", size);
+
+
+    MPI_Finalize();
 }
